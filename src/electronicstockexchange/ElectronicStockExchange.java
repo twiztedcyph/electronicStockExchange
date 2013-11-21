97bc88eaa6e71b46b4c4ddb02434528193b2e353
Ian <Twiz@139.222.218.194>
2013-11-21 15:12:45 +0000

    Project started

---
+++ src/electronicstockexchange/ElectronicStockExchange.java
@@ -0,0 +1,18 @@
+
+package electronicstockexchange;
+
+/**
+ *
+ * @author Twiz
+ */
+public class ElectronicStockExchange
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+}
